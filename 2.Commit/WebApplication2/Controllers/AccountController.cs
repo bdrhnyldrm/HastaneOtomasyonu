824da770aa3dc2e644a28192e77e0894a4e4f3a2
Bedirhan YILDIRIM <yildirimbedirhan007@gmail.com>
2023-12-04 17:56:19 +0300

    2.Commit
    Login Sayfası Geliştirildi.

---
+++ 2.Commit/WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
