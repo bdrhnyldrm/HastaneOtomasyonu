b1b31fcd0c657946520f9245777fd180d3adc239
Bedirhan YILDIRIM <yildirimbedirhan007@gmail.com>
2023-12-05 16:40:38 +0300

    3.Commit
    Login sayfasını çok dilli hale getirdim.

---
+++ 3.Commit/WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+		public IActionResult TurLogin()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(LoginViewModel model)
+		{
+            if (ModelState.IsValid)
+            {
+
+            }
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public IActionResult TurLogin(TurLoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+
+			}
+
+			return View(model);
+		}
+
+		public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ 3.Commit/WebApplication2/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class LoginViewModel
+    {
+        //[Display(Name ="Kullanıcı Adı", Prompt="Johndoe")] Burdaki prompt placeholder yerine geçiyor.
+        [Required(ErrorMessage = "Username is required!!!")]
+        [StringLength(30, ErrorMessage = "Username can be max 30 characters.")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Password is required!!!")]
+        [MinLength(6, ErrorMessage = "Password is can be min 6 characters.")]
+        [MaxLength(16, ErrorMessage = "Password is can be max 16 characters.")]
+        public string Password { get; set; }
+
+    }
+}
