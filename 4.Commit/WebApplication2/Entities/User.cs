de9482a35885da10f1b198c4245cdd27b16c0364
Bedirhan YILDIRIM <yildirimbedirhan007@gmail.com>
2023-12-06 14:17:51 +0300

    4.Commit
    Database bağlantısı kuruldu .

---
+++ 4.Commit/WebApplication2/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Entities
+{
+	[Table("Users")]
+	public class User
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+
+		[StringLength(50)]
+		public string? FullName { get; set; } // Burdaki soru işareti bu property nin null değer alabileceğini gösteriyor.
+		[Required]
+		[StringLength(30)]
+		public string UserName { get; set; }
+		[Required]
+		[StringLength(100)]
+		public string Password { get; set; }
+		public Boolean Locked { get; set; } = false;
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+	}
+}
