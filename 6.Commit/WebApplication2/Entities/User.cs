befb6c84622c1571328fa9d296f3e27af8aafdb7
Bedirhan YILDIRIM <yildirimbedirhan007@gmail.com>
2023-12-09 12:14:13 +0300

    6.Commit
    Roller tanımlandı.

---
+++ 6.Commit/WebApplication2/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Entities
+{
+	[Table("Users")]
+	public class User
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+
+		[StringLength(50)]
+		public string? FullName { get; set; } // Burdaki soru işareti bu property nin null değer alabileceğini gösteriyor.
+		[Required]
+		[StringLength(30)]
+		public string Username { get; set; }
+		
+		[Required]
+		[StringLength(100)]
+		public string Password { get; set; }
+		public Boolean Locked { get; set; } = false;
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+		[Required]
+        [StringLength(50)]
+        public string Role { get; set; } = "user";
+    }
+}
